9d1e338d3b66415c3864718eab7946a3de6ee626
Sarthakdtu <sarthaksrivastavadtu712@gmail.com>
2022-05-22 23:37:08 +0530

    lot of work to do

--- backend/src/main/java/com/foorder/controller/api/UserController.java
+++ backend/src/main/java/com/foorder/controller/api/UserController.java
@@ -1,5 +1,10 @@
 package com.foorder.controller.api;
 
+import com.foorder.common.object.location.ImmutableAddress;
+import com.foorder.common.object.location.ImmutableBlock;
+import com.foorder.common.object.location.ImmutableCity;
+import com.foorder.common.object.location.ImmutableStreet;
+import com.foorder.common.object.user.ImmutableMobileNumber;
 import com.foorder.common.object.user.ImmutableUserProfile;
 import com.foorder.common.object.user.UserProfile;
 import com.foorder.service.UserProfileService;
@@ -8,11 +13,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
+
+import static com.foorder.utils.ResponseBuilder.buildErrorResponse;
+import static com.foorder.utils.ResponseBuilder.buildPostResponse;
 
 @RestController
 @RequestMapping("/api/v1/user-profile")
@@ -79,32 +88,45 @@ public class UserController {
         return userProfiles;
     }
 
+    private ImmutableUserProfile parseUserProfile(HashMap<String, String> req){
+        String username = req.get("username");
+        String cityName = req.get("cityName");
+        String streetName = req.get("streetName");
+        String blockName = req.get("block");
+        String houseNumber = req.get("houseNumber");
+        String mobileNumber = req.getOrDefault("mobileNumber", null);
+        boolean isBot = Boolean.parseBoolean(req.getOrDefault("isBot", String.valueOf(false)));
+        ImmutableCity city = ImmutableCity.builder().name(cityName).build();
+        ImmutableStreet street = ImmutableStreet.builder().name(streetName).build();
+        ImmutableBlock block = ImmutableBlock.builder().name(blockName).build();
+        ImmutableAddress address = ImmutableAddress.builder()
+                .city(city)
+                .street(street)
+                .block(block)
+                .houseNumber(houseNumber)
+                .build();
+        ImmutableMobileNumber mobile = ImmutableMobileNumber.builder().number(mobileNumber).build();
+        return ImmutableUserProfile.builder()
+                .username(username)
+                .address(address)
+                .mobileNumber(mobile)
+                .isBot(isBot)
+                .build();
+    }
+
     @PostMapping("/create")
-    public boolean createUserProfile(@RequestBody HashMap<String, String> req) throws SQLException {
+    public ResponseEntity<?> createUserProfile(@RequestBody HashMap<String, String> req) throws SQLException {
         boolean insert = false;
         try{
-            String username = req.get("username");
-            String cityName = req.get("cityName");
-            String streetName = req.get("streetName");
-            String houseNumber = req.get("houseNumber");
-            String mobileNumber = req.getOrDefault("mobileNumber", null);
-            boolean isBot = Boolean.parseBoolean(req.getOrDefault("isBot", String.valueOf(false)));
-            ImmutableUserProfile userProfile = ImmutableUserProfile.builder()
-                    .username(username)
-                    .cityName(cityName)
-                    .streetName(streetName)
-                    .houseNumber(houseNumber)
-                    .mobileNumber(mobileNumber)
-                    .isBot(isBot)
-                    .build();
+            ImmutableUserProfile userProfile = parseUserProfile(req);
             userProfileService.insertUserProfile(userProfile);
-            insert = true;
+            return buildPostResponse(userProfile.username());
         }
         catch (Exception e){
             logger.error(e.getMessage());
             e.printStackTrace();
+            return buildErrorResponse(e.getMessage(), true);
         }
-        return insert;
     }
 
     @PutMapping("/update")
